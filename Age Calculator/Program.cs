08eab2a69347e676a00377f3e7abdc07d9f89645
ShaunVanBeurden <smkbeurd@avans.nl>
2017-09-05 14:06:20 +0200

    Add project files.

---
+++ Age Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+
+namespace Age_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We maken een nieuw object aan van Birthday
+            Birthday birthday = new Birthday();
+
+            // We vragen de gebruiker om zijn geboortedatum
+            Console.Write("Voer je geboortedatum in (dd-MM-yyyy): ");
+
+            // De geboortedatum wordt ingevoerd en opgeslagen
+            string input = Console.ReadLine();
+
+            // Vervolgens roepen we de calculateBirthday methode aan om de leeftijd te gaan berekenen
+            birthday.calculateBirthday(input);
+
+            // Laat het resultaat zien op het scherm
+            Console.WriteLine("Je leeftijd is " + birthday.Age());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classes/Birthday.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Birthday
+    {
+        int age;
+
+        // Berekent de leeftijd
+        public void calculateBirthday(string input)
+        {
+            DateTime birthDay;
+
+            // Checkt of het parsen wel goed is gebaseerd op de user input
+            bool valid = DateTime.TryParseExact(input, "dd-MM-yyyy",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out birthDay);
+
+            // Als de user input fout is komt de gebruiker in een loop
+            while (!valid)
+            {
+                // Hij moet opnieuw zijn geboortedatum invoeren
+                Console.WriteLine("Je vulde een ongeldige geboortedatum in! Probeer het opnieuw.");
+                Console.Write("Voer je geboortedatum in (dd-MM-yyyy): ");
+                input = Console.ReadLine();
+
+                // Vervolgens wordt gecheckt of de invoer nu wel dd-MM-yyyy is
+                if (DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out birthDay))
+                {
+                    // Zoja dan leaven we de loop
+                    break;
+                }
+            }
+
+            // Stopt de huidige datum in een variabele
+            DateTime currentDate = DateTime.Today;
+
+            // De leeftijd wordt berekent op basis van het huidige jaar - het geboortejaar
+            age = currentDate.Year - birthDay.Year;
+
+            // Ten slotte wordt gecheckt of de persoon al jarig is geweest dit jaar of niet
+            if (currentDate < birthDay.AddYears(age))
+            {
+                // Zonee dan is de persoon 1 jaar jonger omdat hij nog niet jarig is geweest
+                age--;
+            }
+        }
+
+        // Returned de leeftijd
+        public int Age()
+        {
+            return age;
+        }
+    }
+}
